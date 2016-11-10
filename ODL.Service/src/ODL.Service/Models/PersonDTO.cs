a22e3431f4a9c81c62a67ea3371b0117ebb4a7c7
taco <alejandro.coronel@ptj.se>
2016-11-10 16:50:44 +0100

    Minor changes in DTO

--- ODL.Service/src/ODL.Service/Models/PersonDTO.cs
+++ ODL.Service/src/ODL.Service/Models/PersonDTO.cs
@@ -4,8 +4,6 @@
     {
         public int Id { get; set; }
         public string Personnummer { get; set; }
-        public string PersonAlias { get; set; }
-        public string KonsultAlias { get; set; }
         public string Namn { get; set; }
     }
 }
