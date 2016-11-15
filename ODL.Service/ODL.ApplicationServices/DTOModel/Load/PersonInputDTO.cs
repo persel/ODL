e7e23a3d98add78dd00ad10be540803726ab2f35
taco <alejandro.coronel@ptj.se>
2016-11-15 09:21:38 +0100

    Addd the Person Input model

---
+++ ODL.Service/ODL.ApplicationServices/DTOModel/Load/PersonInputDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ODL.DomainModel.Common;
+using ODL.DomainModel.Person;
+
+namespace ODL.ApplicationServices.DTOModel.Load
+{
+    public class PersonInputDTO
+    {
+        public string SystemId { get; set; }
+        public string Personnummer { get; set; }
+        public string Namn { get; set; }
+        public string Fornamn { get; set; }
+        public string Mellannamn { get; set; }
+        public string Efternamn { get; set; }
+        public string Alias { get; set; }
+        public string AnstalldTyp { get; set; }
+        public bool IsKonsult { get; set; }
+        public bool IsAnstalld { get; set; }
+        public string UppdateradDatum { get; set; }
+        public string UppdateradAv { get; set; }
+        public string SkapadDatum { get; set; }
+        public string SkapadAv { get; set; }
+        
+    }
+}
