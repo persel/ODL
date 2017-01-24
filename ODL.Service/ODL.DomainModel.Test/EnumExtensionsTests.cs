da7d3eafc0baec5419f6978c6214f6a464e49646
taco <alejandro.coronel@ptj.se>
2017-01-24 09:27:43 +0100

    La in Enumextensions och attributet Visningstext

---
+++ ODL.Service/ODL.DomainModel.Test/EnumExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using ODL.DomainModel.Adress;
+using ODL.DomainModel.Common;
+
+namespace ODL.ApplicationServices.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        [Test]
+        public void Visningstext_Returnerar_Text()
+        {
+            Assert.That(TestEnumMedTexter.Typ1.Visningstext(), Is.EqualTo("Typ 1"));
+            Assert.That(TestEnumMedTexter.Typ3.Visningstext(), Is.EqualTo("Typ 3"));
+        }
+
+        [Test]
+        public void Visningstext_Returnerar_Null_Om_Text_Saknas()
+        {
+            Assert.That(TestEnumUtanTexter.Typ2.Visningstext(), Is.Null);
+        }
+    }
+
+    public enum TestEnumMedTexter
+    {
+        [Visningstext("Typ 1")]
+        Typ1,
+
+        [Visningstext("Typ 2")]
+        Typ2,
+
+        [Visningstext("Typ 3")]
+        Typ3
+    }
+
+    public enum TestEnumUtanTexter
+    {
+        Typ1,
+        Typ2,
+        Typ3
+    }
+}
---
+++ ODL.Service/ODL.DomainModel/Common/VisningstextAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ODL.DomainModel.Common
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class VisningstextAttribute : Attribute
+    {
+        public string Text { get; set; }
+        public VisningstextAttribute(string text)
+        {
+            Text = text;
+        }
+    }
+}
