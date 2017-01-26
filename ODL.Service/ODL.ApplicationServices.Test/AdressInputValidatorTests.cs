68b13b96c3c487c960ed51d24d1d1b75821f8e0e
mahlin <marie.ahlin@ptj.se>
2017-01-26 14:39:34 +0100

    .

--- ODL.Service/ODL.ApplicationServices.Test/AdressInputValidatorTests.cs
+++ ODL.Service/ODL.ApplicationServices.Test/AdressInputValidatorTests.cs
@@ -11,31 +11,102 @@ namespace ODL.ApplicationServices.Test
     public class AdressInputValidatorTests
     {
         [Test]
-        public void TestValidate()
+        public void TestValidatePersonAdressTelefon()
         {
-          
-            var personAdress = new PersonAdressInputDTO
-            {
-                Personnummer = "123456-7788",
-                AdressVariant = "Mobil Arbete",
-                GatuadressInput = null,
-                MailInput = null,
-                TelefonInput  = new TelefonInputDTO {Telefonnummer = ""},
-                SystemId = null,
-                UppdateradDatum = "2017-01-20",
-                UppdateradAv = "MAH",
-                SkapadDatum = "2017 - 01 - 20 12:00",
-                SkapadAv = "MAH"
-            };
-
+            var personAdress = CreatePersonAdress();
 
             var brokenRules = new PersonAdressInputValidator().Validate(personAdress);
             new AdressInputValidator().Validate(personAdress, brokenRules);
 
             Assert.That(brokenRules.Count, Is.EqualTo(3));
 
-            Assert.That(brokenRules.Any(ve => ve.Message.Equals("Fältet 'AdressInputDTO.SkapadDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
-            Assert.That(brokenRules.Any(ve => ve.Message.Equals("Fältet 'AdressInputDTO.UppdateradDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
+            Assert.That(
+                brokenRules.Any(
+                    ve =>
+                        ve.Message.Equals(
+                            "Fältet 'AdressInputDTO.SkapadDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
+            Assert.That(
+                brokenRules.Any(
+                    ve =>
+                        ve.Message.Equals(
+                            "Fältet 'AdressInputDTO.UppdateradDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
+        }
+
+        [Test]
+        public void TestValidateOrganisationAdress()
+        {
+
+            var organisationAdress = CreateOrganisationAdress();
+
+            var brokenRules = new OrganisationAdressInputValidator().Validate(organisationAdress);
+            new AdressInputValidator().Validate(organisationAdress, brokenRules);
+
+            Assert.That(brokenRules.Count, Is.EqualTo(3));
+
+            //Assert.That(brokenRules.Any(ve =>ve.Message.Equals("Fältet 'AdressInputDTO.SkapadDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
+            //Assert.That(brokenRules.Any(ve =>ve.Message.Equals("Fältet 'AdressInputDTO.UppdateradDatum' har ej korrekt datumformat ('yyyy-MM-dd HH:mm').")));
+        }
+
+        private PersonAdressInputDTO CreatePersonAdress(GatuadressInputDTO gatuadressInput, MailInputDTO mailInput, TelefonInputDTO telefonInput)
+        {
+            var personAdress = new PersonAdressInputDTO();
+            if (gatuadressInput != null)
+            {
+                var personGatuadress = new PersonAdressInputDTO
+                {
+                    Personnummer = "123456-7788",
+                    AdressVariant = "Mobil Arbete",
+                    GatuadressInput = null,
+                    MailInput = null,
+                    TelefonInput = new TelefonInputDTO { Telefonnummer = "" },
+                    SystemId = null,
+                    UppdateradDatum = "2017-01-20",
+                    UppdateradAv = "MAH",
+                    SkapadDatum = "2017 - 01 - 20 12:00",
+                    SkapadAv = "MAH"
+                };
+                personAdress = personGatuadress;
+            }
+            if (mailInput != null)
+            {
+                var personEpostadress = new PersonAdressInputDTO
+                {
+                    Personnummer = "123456-7788",
+                    AdressVariant = "Mobil Arbete",
+                    GatuadressInput = null,
+                    MailInput = null,
+                    TelefonInput = new TelefonInputDTO { Telefonnummer = "" },
+                    SystemId = null,
+                    UppdateradDatum = "2017-01-20",
+                    UppdateradAv = "MAH",
+                    SkapadDatum = "2017 - 01 - 20 12:00",
+                    SkapadAv = "MAH"
+                };
+                personAdress = personEpostadress;
+            }
+            if (telefonInput != null)
+            {
+                var personTelefon = new PersonAdressInputDTO
+                {
+                    Personnummer = "123456-7788",
+                    AdressVariant = "Mobil Arbete",
+                    GatuadressInput = null,
+                    MailInput = null,
+                    TelefonInput = new TelefonInputDTO { Telefonnummer = "" },
+                    SystemId = null,
+                    UppdateradDatum = "2017-01-20",
+                    UppdateradAv = "MAH",
+                    SkapadDatum = "2017 - 01 - 20 12:00",
+                    SkapadAv = "MAH"
+                };
+                personAdress =  personTelefon;
+            }
+            return personAdress;
+        }
+
+        private OrganisationAdressInputDTO CreateOrganisationAdress()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
