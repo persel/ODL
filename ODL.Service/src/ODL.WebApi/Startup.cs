83d0412c22e8b23f96df04266e484845176cdd80
taco <alejandro.coronel@ptj.se>
2017-03-15 13:58:39 +0100

    testing the build

--- ODL.Service/src/ODL.WebApi/Startup.cs
+++ ODL.Service/src/ODL.WebApi/Startup.cs
@@ -33,7 +33,7 @@ namespace ODL.Service
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; } 
 
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
