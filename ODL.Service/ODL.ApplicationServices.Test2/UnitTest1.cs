d05afdc91d5de767efbecb7ae8c866f8897d1c3b
Per Selin <pse@ptj.se>
2017-03-21 13:03:04 +0100

    Lägger till nunit console

--- ODL.Service/ODL.ApplicationServices.Test2/UnitTest1.cs
+++ ODL.Service/ODL.ApplicationServices.Test2/UnitTest1.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace ODL.ApplicationServices.Test2
 {
-    [TestClass]
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //        Assert.AreEqual("", "");
+    //    }
+    //}
+
+
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void TestValidatePersonAdressGatuAdress()
         {
             Assert.AreEqual("", "");
         }
     }
+
 }
