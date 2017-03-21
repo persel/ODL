e17edeb7b05457b2977d1e109fa6a77ab6debc70
Per Selin <pse@ptj.se>
2017-03-21 13:35:19 +0100

    tar bort DB testen

--- ODL.Service/test/ODL.ApplicationServices.Test/AdressServiceTests.cs
+++ ODL.Service/test/ODL.ApplicationServices.Test/AdressServiceTests.cs
@@ -1,71 +1,71 @@
-﻿using System.Data.Entity;
-using Microsoft.Extensions.Logging;
-using Moq;
-using NUnit.Framework;
-using ODL.ApplicationServices;
-using ODL.ApplicationServices.DTOModel;
-using ODL.ApplicationServices.DTOModel.Load;
-using ODL.DataAccess;
-using ODL.DataAccess.Repositories;
+﻿//using System.Data.Entity;
+//using Microsoft.Extensions.Logging;
+//using Moq;
+//using NUnit.Framework;
+//using ODL.ApplicationServices;
+//using ODL.ApplicationServices.DTOModel;
+//using ODL.ApplicationServices.DTOModel.Load;
+//using ODL.DataAccess;
+//using ODL.DataAccess.Repositories;
 
-[TestFixture]
-public class AdressServiceTest
-{
-    protected ODLDbContext context;
-    protected DbContextTransaction transaction;
+//[TestFixture]
+//public class AdressServiceTest
+//{
+//    protected ODLDbContext context;
+//    protected DbContextTransaction transaction;
 
-    [SetUp]
-    public void TransactionTestStart()
-    {
-        Database.SetInitializer<ODLDbContext>(null);
-        context = new ODLDbContext();
-        transaction = context.Database.BeginTransaction();
-    }
+//    [SetUp]
+//    public void TransactionTestStart()
+//    {
+//        Database.SetInitializer<ODLDbContext>(null);
+//        context = new ODLDbContext();
+//        transaction = context.Database.BeginTransaction();
+//    }
 
 
-    [Test]
-    public void SparaPersonAdress_WhenNew_ThenSaved()
-    {
+//    [Test]
+//    public void SparaPersonAdress_WhenNew_ThenSaved()
+//    {
 
-        var personRepositoryMock = new Mock<IPersonRepository>().Object;
-        var adressRepositoryMock = new Mock<IAdressRepository>().Object;
-        var loggerMock = new Mock<ILogger<AdressService>>().Object;
+//        var personRepositoryMock = new Mock<IPersonRepository>().Object;
+//        var adressRepositoryMock = new Mock<IAdressRepository>().Object;
+//        var loggerMock = new Mock<ILogger<AdressService>>().Object;
 
-        var service = new AdressService(new AdressRepository(context),  new PersonRepository(context), new OrganisationRepository(context), new AdressVariantRepository(context), loggerMock);
-        var personGatauAdress = new PersonAdressInputDTO
-        {
-            Personnummer = "197012123456",
-            AdressVariant = "LeveransAdress",
-            GatuadressInput = new GatuadressInputDTO
-            {
-                AdressRad1 = "Gatan 2",
-                AdressRad2 = "",
-                AdressRad3 = "",
-                AdressRad4 = "",
-                AdressRad5 = "",
-                Postnummer = 74141,
-                Stad = "Knivsta",
-                Land = "USA"
-            },
-            MailInput = null,
-            TelefonInput = null,
-            SystemId = null,
-            UppdateradDatum = "2017-01-20 12:00",
-            UppdateradAv = "MAH",
-            SkapadDatum = "2017-01-20 12:00",
-            SkapadAv = "MAH"
-        };
-        service.SparaPersonAdress(personGatauAdress);
+//        var service = new AdressService(new AdressRepository(context),  new PersonRepository(context), new OrganisationRepository(context), new AdressVariantRepository(context), loggerMock);
+//        var personGatauAdress = new PersonAdressInputDTO
+//        {
+//            Personnummer = "197012123456",
+//            AdressVariant = "LeveransAdress",
+//            GatuadressInput = new GatuadressInputDTO
+//            {
+//                AdressRad1 = "Gatan 2",
+//                AdressRad2 = "",
+//                AdressRad3 = "",
+//                AdressRad4 = "",
+//                AdressRad5 = "",
+//                Postnummer = 74141,
+//                Stad = "Knivsta",
+//                Land = "USA"
+//            },
+//            MailInput = null,
+//            TelefonInput = null,
+//            SystemId = null,
+//            UppdateradDatum = "2017-01-20 12:00",
+//            UppdateradAv = "MAH",
+//            SkapadDatum = "2017-01-20 12:00",
+//            SkapadAv = "MAH"
+//        };
+//        service.SparaPersonAdress(personGatauAdress);
 
-        var sparadPersonAdress = service.GetAdresserPerPersonnummer("197012123456");
-        Assert.That(sparadPersonAdress, Is.Not.Null);
-    }
+//        var sparadPersonAdress = service.GetAdresserPerPersonnummer("197012123456");
+//        Assert.That(sparadPersonAdress, Is.Not.Null);
+//    }
 
-    [TearDown]
-    public void TransactionTestEnd()
-    {
-        transaction.Rollback();
-        transaction.Dispose();
-        context.Dispose();
-    }
-}
+//    [TearDown]
+//    public void TransactionTestEnd()
+//    {
+//        transaction.Rollback();
+//        transaction.Dispose();
+//        context.Dispose();
+//    }
+//}
--- ODL.Service/test/ODL.ApplicationServices.Test/PersonServiceTests.cs
+++ ODL.Service/test/ODL.ApplicationServices.Test/PersonServiceTests.cs
@@ -1,61 +1,61 @@
 ﻿
-using System.Data.Entity;
-using Microsoft.Extensions.Logging;
-using Moq;
-using NUnit.Framework;
-using ODL.ApplicationServices;
-using ODL.ApplicationServices.DTOModel.Load;
-using ODL.DataAccess;
-using ODL.DataAccess.Repositories;
+//using System.Data.Entity;
+//using Microsoft.Extensions.Logging;
+//using Moq;
+//using NUnit.Framework;
+//using ODL.ApplicationServices;
+//using ODL.ApplicationServices.DTOModel.Load;
+//using ODL.DataAccess;
+//using ODL.DataAccess.Repositories;
 
-[TestFixture]
-public class PersonServiceTests
-{
-    protected ODLDbContext context;
-    protected DbContextTransaction transaction;
+//[TestFixture]
+//public class PersonServiceTests
+//{
+//    protected ODLDbContext context;
+//    protected DbContextTransaction transaction;
 
-    [SetUp]
-    public void TransactionTestStart()
-    {
-        Database.SetInitializer<ODLDbContext>(null);
-        context = new ODLDbContext();
-        transaction = context.Database.BeginTransaction();
-    }
+//    [SetUp]
+//    public void TransactionTestStart()
+//    {
+//        Database.SetInitializer<ODLDbContext>(null);
+//        context = new ODLDbContext();
+//        transaction = context.Database.BeginTransaction();
+//    }
 
 
-    [Test]
-    public void SparaPerson_WhenNew_ThenSaved()
-    {
+//    [Test]
+//    public void SparaPerson_WhenNew_ThenSaved()
+//    {
         
-        var organisationRepositoryMock = new Mock<IOrganisationRepository>().Object;
-        var avtalRepositoryMock = new Mock<IAvtalRepository>().Object;
-        var loggerMock = new Mock<ILogger<PersonService>>().Object;
+//        var organisationRepositoryMock = new Mock<IOrganisationRepository>().Object;
+//        var avtalRepositoryMock = new Mock<IAvtalRepository>().Object;
+//        var loggerMock = new Mock<ILogger<PersonService>>().Object;
 
-        var service = new PersonService(new PersonRepository(context), organisationRepositoryMock, avtalRepositoryMock, loggerMock);
-        var person = new PersonInputDTO
-        {
-            SystemId = "435345",
-            Fornamn = "Anna",
-            Efternamn = "Nilsson",
-            Personnummer = "198002254543",
-            SkapadAv = "MEH",
-            SkapadDatum = "2005-01-02 12:00",
-            UppdateradAv = "MEH",
-            UppdateradDatum = "2017-02-01 12:00"
-        };
-        service.SparaPerson(person);
+//        var service = new PersonService(new PersonRepository(context), organisationRepositoryMock, avtalRepositoryMock, loggerMock);
+//        var person = new PersonInputDTO
+//        {
+//            SystemId = "435345",
+//            Fornamn = "Anna",
+//            Efternamn = "Nilsson",
+//            Personnummer = "198002254543",
+//            SkapadAv = "MEH",
+//            SkapadDatum = "2005-01-02 12:00",
+//            UppdateradAv = "MEH",
+//            UppdateradDatum = "2017-02-01 12:00"
+//        };
+//        service.SparaPerson(person);
 
-        var sparadPerson = service.GetPersonByPersonnummer("198002254543");
-        Assert.That(sparadPerson, Is.Not.Null);
-    }
+//        var sparadPerson = service.GetPersonByPersonnummer("198002254543");
+//        Assert.That(sparadPerson, Is.Not.Null);
+//    }
 
-    [TearDown]
-    public void TransactionTestEnd()
-    {
-        transaction.Rollback();
-        transaction.Dispose();
-        context.Dispose();
-    }
+//    [TearDown]
+//    public void TransactionTestEnd()
+//    {
+//        transaction.Rollback();
+//        transaction.Dispose();
+//        context.Dispose();
+//    }
 
 
-}
+//}
