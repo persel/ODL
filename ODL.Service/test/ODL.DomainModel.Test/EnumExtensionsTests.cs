01ba4157756df6f87644d2b480c0ae4e5109a008
Per Selin <pse@ptj.se>
2017-03-21 13:58:49 +0100

    .

--- ODL.Service/test/ODL.DomainModel.Test/EnumExtensionsTests.cs
+++ ODL.Service/test/ODL.DomainModel.Test/EnumExtensionsTests.cs
@@ -1,9 +1,10 @@
 ﻿using ODL.DomainModel.Adress;
 using ODL.DomainModel.Common;
+using NUnit.Framework;
 
 namespace ODL.ApplicationServices.Test
 {
-    using NUnit.Framework;
+    
 
     [TestFixture]
     public class EnumExtensionsTests
