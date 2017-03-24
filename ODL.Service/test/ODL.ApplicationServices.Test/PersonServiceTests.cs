1ecdf74a4bac0fb3e88eb690c9d93b02b5d20d59
Per Selin <pse@ptj.se>
2017-03-24 09:06:12 +0100

    lite mer test fall

--- ODL.Service/test/ODL.ApplicationServices.Test/PersonServiceTests.cs
+++ ODL.Service/test/ODL.ApplicationServices.Test/PersonServiceTests.cs
@@ -1,123 +1,62 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Data.Entity;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
-using ODL.ApplicationServices;
 using ODL.ApplicationServices.DTOModel.Load;
 using ODL.DataAccess;
 using ODL.DataAccess.Repositories;
-using ODL.DomainModel.Organisation;
-using ODL.DomainModel.Person;
 
-
-[TestFixture]
-public class PersonServiceTests
+namespace ODL.ApplicationServices.Test
 {
-    protected ODLDbContext context;
-    protected DbContextTransaction transaction;
-
-    //[SetUp]
-    //public void TransactionTestStart()
-    //{
-    //    Database.SetInitializer<ODLDbContext>(null);
-    //    context = new ODLDbContext();
-    //    transaction = context.Database.BeginTransaction();
-    //}
-
-    [Test]
-    public void SparaPerson_WhenNew_ThenSaved()
+    [TestFixture]
+    public class PersonServiceTests
     {
-        
-        var personRepositoryMock = new Mock<IPersonRepository>();
+        protected ODLDbContext context;
+        protected DbContextTransaction transaction;
 
-        //Setup 
-        var personlist = new List<Person>();
-        personRepositoryMock.Setup(m => m.Add((It.IsAny<Person>()))).Callback<Person>(personlist.Add);
-        personRepositoryMock.Setup(m => m.GetByPersonnummer(It.IsAny<string>())).Returns(new Person() );
-
-        var organisationRepositoryMock = new Mock<IOrganisationRepository>().Object;
-        var avtalRepositoryMock = new Mock<IAvtalRepository>().Object;
-        var loggerMock = new Mock<ILogger<PersonService>>().Object;
-
-        var service = new PersonService( personRepositoryMock.Object, organisationRepositoryMock, avtalRepositoryMock, loggerMock);
-
-        var person = new PersonInputDTO
+        [SetUp]
+        public void TransactionTestStart()
         {
-            SystemId = "435345",
-            Fornamn = "Anna",
-            Efternamn = "Nilsson",
-            Personnummer = "198002254543",
-            SkapadAv = "MEH",
-            SkapadDatum = "2005-01-02 12:00",
-            UppdateradAv = "MEH",
-            UppdateradDatum = "2017-02-01 12:00"
-        };
-        service.SparaPerson(person);
-        var sparadPerson = service.GetPersonByPersonnummer("198002254543");
-
-      
-        Assert.That(sparadPerson, Is.Not.Null);
-    }
+            Database.SetInitializer<ODLDbContext>(null);
+            context = new ODLDbContext();
+            transaction = context.Database.BeginTransaction();
+        }
 
 
-    //[Test]
-    //public void SparaPerson_WhenNew_ThenSaved()
-    //{
+        [Test]
+        public void SparaPerson_WhenNew_ThenSaved()
+        {
 
-    //    var organisationRepositoryMock = new Mock<IOrganisationRepository>().Object;
-    //    var avtalRepositoryMock = new Mock<IAvtalRepository>().Object;
-    //    var loggerMock = new Mock<ILogger<PersonService>>().Object;
+            var organisationRepositoryMock = new Mock<IOrganisationRepository>().Object;
+            var avtalRepositoryMock = new Mock<IAvtalRepository>().Object;
+            var loggerMock = new Mock<ILogger<PersonService>>().Object;
 
-    //    var service = new PersonService(new PersonRepository(context), organisationRepositoryMock, avtalRepositoryMock, loggerMock);
-    //    var person = new PersonInputDTO
-    //    {
-    //        SystemId = "435345",
-    //        Fornamn = "Anna",
-    //        Efternamn = "Nilsson",
-    //        Personnummer = "198002254543",
-    //        SkapadAv = "MEH",
-    //        SkapadDatum = "2005-01-02 12:00",
-    //        UppdateradAv = "MEH",
-    //        UppdateradDatum = "2017-02-01 12:00"
-    //    };
-    //    service.SparaPerson(person);
+            var service = new PersonService(new PersonRepository(context), organisationRepositoryMock, avtalRepositoryMock, loggerMock);
+            var person = new PersonInputDTO
+            {
+                SystemId = "435345",
+                Fornamn = "Anna",
+                Efternamn = "Nilsson",
+                Personnummer = "198002254543",
+                SkapadAv = "MEH",
+                SkapadDatum = "2005-01-02 12:00",
+                UppdateradAv = "MEH",
+                UppdateradDatum = "2017-02-01 12:00"
+            };
+            service.SparaPerson(person);
 
-    //    var sparadPerson = service.GetPersonByPersonnummer("198002254543");
-    //    Assert.That(sparadPerson, Is.Not.Null);
-    //}
+            var sparadPerson = service.GetPersonByPersonnummer("198002254543");
+            Assert.That(sparadPerson, Is.Not.Null);
+        }
 
-    //[TearDown]
-    //public void TransactionTestEnd()
-    //{
-    //    transaction.Rollback();
-    //    transaction.Dispose();
-    //    context.Dispose();
-    //}
-
-
-}
-
-public class MockStore
-{
-
-    public List<Person> Person { get; set; }
-    public List<Avtal> Avtal { get; set; }
-
-    public List<Organisation> Organisation { get; set; }
-
-
-    public MockStore()
-    {
-        this.Person = new List<Person>();
-
-        this.Avtal = new List<Avtal>();
-
-        this.Organisation = new List<Organisation>();
+        [TearDown]
+        public void TransactionTestEnd()
+        {
+            transaction.Rollback();
+            transaction.Dispose();
+            context.Dispose();
+        }
 
     }
-
 }
+
