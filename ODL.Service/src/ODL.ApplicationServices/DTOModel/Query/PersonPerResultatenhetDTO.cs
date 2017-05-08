a84c9fdc89e7b5b5aaaf97de744d51fd65e38fc0
taco <alejandro.coronel@ptj.se>
2017-05-08 10:53:04 +0200

    Ny query

---
+++ ODL.Service/src/ODL.ApplicationServices/DTOModel/Query/PersonPerResultatenhetDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ODL.ApplicationServices.DTOModel.Query
+{
+    public class PersonPerResultatenhetDTO
+    {
+        public int Id { get; set; }
+        public string Personnummer { get; set; }
+        public string Namn { get; set; }
+        public string KostnadsstalleNr { get; set; }
+        public bool? Resultatenhetansvarig { get; set; }
+    }
+    
+}
---
+++ ODL.Service/src/ODL.ApplicationServices/Queries/AnstalldaPerKostnadsstalleQuery.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ODL.ApplicationServices.DTOModel.Query;
+using ODL.DataAccess;
+using ODL.DomainModel;
+using ODL.DomainModel.Adress;
+using ODL.DomainModel.Organisation;
+using ODL.DomainModel.Person;
+
+namespace ODL.ApplicationServices.Queries
+{
+    internal class AnstalldaPerKostnadsstalleQuery
+    {
+        public IContext DbContext { get; set; }
+
+        public AnstalldaPerKostnadsstalleQuery(IContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public List<PersonPerResultatenhetDTO> Execute(string kstNr)
+        {
+            var allaAvtal = DbContext.DbSet<Avtal>();
+            var allaAnstalldAvtal = DbContext.DbSet<AnstalldAvtal>(); // Denna kan utelämnas och joinas mha navigation property, men det komplicerar det genrerade SQL-scriptet
+            var allaKonsultAvtal = DbContext.DbSet<KonsultAvtal>();
+            var personer = DbContext.DbSet<Person>();
+            var organisationer = DbContext.DbSet<Organisation>();
+            var adresser = DbContext.DbSet<Adress>();
+            var allaOrganisationsAvtal = DbContext.DbSet<OrganisationAvtal>();
+            var allaOrganisationsAdresser = DbContext.DbSet<OrganisationAdress>();// Denna kan utelämnas och joinas mha navigation property, men det komplicerar det genrerade SQL-scriptet
+            
+
+            var projektion = from person in personer
+
+                from anstalldAvtal in allaAnstalldAvtal
+                from konsultAvtal in allaKonsultAvtal
+                from avtal in allaAvtal
+
+                //join anstalldAvtal in allaAnstalldAvtal on person.Id equals anstalldAvtal.PersonId
+                //join avtal in allaAvtal on anstalldAvtal.AvtalId equals avtal.Id
+                join organisationsAvtal in allaOrganisationsAvtal on avtal.Id equals organisationsAvtal.AvtalId
+                join organisation in organisationer on organisationsAvtal.OrganisationId equals organisation.Id
+                where organisation.Resultatenhet.KstNr == kstNr && (avtal.Id == anstalldAvtal.AvtalId || avtal.Id == konsultAvtal.AvtalId)
+                             select new PersonPerResultatenhetDTO { Id = person.Id, KostnadsstalleNr = kstNr, Personnummer = person.Personnummer, Namn = $"{person.Fornamn} {person.Efternamn}", Resultatenhetansvarig = avtal.Ansvarig};
+
+
+            return projektion.ToList(); // Ta bort dubletter
+
+        }
+    }
+}
