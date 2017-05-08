316df9d5f0572bc27f4a43e809706cb33ebd9f90
taco <alejandro.coronel@ptj.se>
2017-05-08 12:53:17 +0200

    Ändrade i en fråga

--- ODL.Service/src/ODL.ApplicationServices/Queries/PersonerPerResultatenhetQuery.cs
+++ ODL.Service/src/ODL.ApplicationServices/Queries/PersonerPerResultatenhetQuery.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using ODL.ApplicationServices.DTOModel.Query;
 using ODL.DataAccess;
 using ODL.DomainModel;
-using ODL.DomainModel.Adress;
 using ODL.DomainModel.Organisation;
 using ODL.DomainModel.Person;
 
@@ -22,27 +20,28 @@ namespace ODL.ApplicationServices.Queries
         public List<PersonPerResultatenhetDTO> Execute(string kstNr)
         {
             var allaAvtal = DbContext.DbSet<Avtal>();
-            var allaAnstalldAvtal = DbContext.DbSet<AnstalldAvtal>(); // Denna kan utelämnas och joinas mha navigation property, men det komplicerar det genrerade SQL-scriptet
-            var allaKonsultAvtal = DbContext.DbSet<KonsultAvtal>();
             var personer = DbContext.DbSet<Person>();
             var organisationer = DbContext.DbSet<Organisation>();
             var allaOrganisationsAvtal = DbContext.DbSet<OrganisationAvtal>();
-            
-            var projektion = from person in personer
 
-                from anstalldAvtal in allaAnstalldAvtal
-                from konsultAvtal in allaKonsultAvtal
-                from avtal in allaAvtal
-
-                //join anstalldAvtal in allaAnstalldAvtal on person.Id equals anstalldAvtal.PersonId
-                //join avtal in allaAvtal on anstalldAvtal.AvtalId equals avtal.Id
+            var projektionAnstallda = from person in personer
+                join avtal in allaAvtal on person.Id equals avtal.AnstalldAvtal.PersonId
                 join organisationsAvtal in allaOrganisationsAvtal on avtal.Id equals organisationsAvtal.AvtalId
                 join organisation in organisationer on organisationsAvtal.OrganisationId equals organisation.Id
-                where organisation.Resultatenhet.KstNr == kstNr && (avtal.Id == anstalldAvtal.AvtalId || avtal.Id == konsultAvtal.AvtalId)
-                             select new PersonPerResultatenhetDTO { Id = person.Id, KostnadsstalleNr = kstNr, Personnummer = person.Personnummer, Namn = $"{person.Fornamn} {person.Efternamn}", Resultatenhetansvarig = avtal.Ansvarig};
+                where organisation.Resultatenhet.KstNr == kstNr
+                select new PersonPerResultatenhetDTO { Id = person.Id, KostnadsstalleNr = kstNr, Personnummer = person.Personnummer, Namn = $"{person.Fornamn} {person.Efternamn}", Resultatenhetansvarig = avtal.Ansvarig};
+
+            var projektionKonsulter = from person in personer
+                join avtal in allaAvtal on person.Id equals avtal.KonsultAvtal.PersonId
+                join organisationsAvtal in allaOrganisationsAvtal on avtal.Id equals organisationsAvtal.AvtalId
+                join organisation in organisationer on organisationsAvtal.OrganisationId equals organisation.Id
+                where organisation.Resultatenhet.KstNr == kstNr
+                select new PersonPerResultatenhetDTO { Id = person.Id, KostnadsstalleNr = kstNr, Personnummer = person.Personnummer, Namn = $"{person.Fornamn} {person.Efternamn}", Resultatenhetansvarig = avtal.Ansvarig };
 
 
-            return projektion.ToList(); // Ta bort dubletter
+            projektionAnstallda.ToList().AddRange(projektionKonsulter.ToList()); 
+
+            return projektionAnstallda.ToList(); // TODO: Ta bort dubletter, ansvarig = true om både true/false finns
 
         }
     }
